5993b5141736afe74f1584162e95a50f5e62cf9b
KaidalovIlya <kaydalov.2026@mail.ru>
2024-11-04 21:47:09 +0500

    лекция 4 и 5

---
+++ Todo.ServerApp/Data/Services/MSSQLDataService.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Todo.ServerApp.Data.Interfaces;
+
+namespace Todo.ServerApp.Data.Services
+{
+    public class MSSQLDataService(ApplicationDbContext context) : IDataService
+    {
+        public async Task<IEnumerable<TaskItem>> GetTaskItemsAsync()
+        {
+            return await context.TaskItems.ToArrayAsync();
+        }
+    }
+}
